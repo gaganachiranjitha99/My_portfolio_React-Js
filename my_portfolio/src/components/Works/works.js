a1a0a4c3043ac6d7e2daee6576f1dac0c5830cb0
Gagana Chiranjitha <103405715+gaganachiranjitha99@users.noreply.github.com>
2023-09-06 20:47:33 +0530

    updated

--- my_portfolio/src/components/Works/works.js
+++ my_portfolio/src/components/Works/works.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import './works.css';
 
 const Works = () => {
   return (
-    <section id="works"></section>
+    <section id="works">
+        <h2 className="worksTitle">My Portfolio</h2>
+        <span className='worksDesc'>I'm an enthusiastic and highly skilled software developer with a deep passion for creating elegant and efficient solutions. With Java,Spring Boot,Python,React Js,Node Js and Flutter, I've had the privilege of working on a diverse range of projects, gaining expertise in various technologies, and delivering high-quality software solutions.</span>
+
+
+    </section>
   )
 }
 
